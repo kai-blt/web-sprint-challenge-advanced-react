f5b3c9f6b36c2bca198acd6a6030cc07e0d0867d
kai-blt <blaitong@gmail.com>
2020-10-30 09:02:36 -0700

    added code comments

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 export default function useForm(initialValues) {
+    //Holding state for form values and success message
     const [values, setValues] = useState(initialValues);
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
+    //Change handler
     const handleChanges = (e) => {
         setValues({ ...values, [e.target.name]: e.target.value });
     };
 
+    //Submit handler
     const handleSubmit = (e) => {
         e.preventDefault();
         setShowSuccessMessage(true);
